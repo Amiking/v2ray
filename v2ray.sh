a919ac1372384f82ccdcc439af17e5a85767a112
ComeBey <46746974+ComeBey@users.noreply.github.com>
2019-02-25 16:35:01 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -753,7 +753,7 @@ install_v2ray() {
 		cp -rf $(pwd)/* /etc/v2ray/comebey/v2ray
 	else
 		pushd /tmp
-		git clone https://raw.githubusercontent.com/ComeBey/v2ray/master/v2ray.sh -b "$_gitbranch" /etc/v2ray/233boy/v2ray
+		git clone https://raw.githubusercontent.com/ComeBey/v2ray/master/v2ray.sh -b "$_gitbranch" /etc/v2ray/comebey/v2ray
 		popd
 
 	fi
