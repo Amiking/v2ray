339c69b233f64d6df019214e303fd521b9827f02
ComeBey <46746974+ComeBey@users.noreply.github.com>
2019-02-25 21:46:52 +0800

    Update v2ray.sh

--- v2ray.sh
+++ v2ray.sh
@@ -48,8 +48,8 @@ fi
 uuid=$(cat /proc/sys/kernel/random/uuid)
 old_id="e55c8d17-2cf3-b21a-bcf1-eeacb011ed79"
 v2ray_server_config="/etc/v2ray/config.json"
-v2ray_client_config="/etc/v2ray/233blog_v2ray_config.json"
-backup="/etc/v2ray/233blog_v2ray_backup.conf"
+v2ray_client_config="/etc/v2ray/comebey_v2ray_config.json"
+backup="/etc/v2ray/comebey_v2ray_backup.conf"
 _v2ray_sh="/usr/local/sbin/v2ray"
 systemd=true
 # _test=true
